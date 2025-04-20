f628875e550466f6927085b1cdec21cb8c6cd06c
Ayush Vaibhav <softprotechcoder@outlook.com>
2025-04-20 09:32:47 +0530

    Created Components implemented routings homepage inProgress.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterModule, RouterOutlet } from '@angular/router';
+import { HomeComponent } from "../../pages/home/home.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Angular_Developer_Portfolio';
+  currentYear: number = new Date().getFullYear();
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from '../../pages/home/home.component';
+import { AboutComponent } from '../../pages/about/about.component';
+import { ProjectsComponent } from '../../pages/projects/projects.component';
+import { ContactsComponent } from '../../pages/contacts/contacts.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', redirectTo: 'home', pathMatch: 'full'},
+    {path: 'home',  component : HomeComponent},
+    {path: 'about', component : AboutComponent},
+    {path: 'projects', component : ProjectsComponent},
+    {path: 'contact', component: ContactsComponent},
+    {path: '**', redirectTo: ''}  //fallback to home
+];
